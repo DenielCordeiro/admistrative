1a4f317e5b6728d9c120a87e687aab9a860cc9a4
Daniel de Souza Cordeiro <Daniel.Cordeiro@ext.rumolog.com>
2025-06-25 13:04:54 -0300

    <feat>[annualLeaves]: start class and funcion, still under construction

--- annual-leave/js/annual-leave.js
+++ annual-leave/js/annual-leave.js
@@ -3,6 +3,26 @@ class AnnualLeave {
         console.log('Classe banco de horas iniciada!');
         
     }
+
+    setMaker() {
+        try {
+            const dataExemplo = new Date.now();
+            const hourMarked = dataExemplo.getHours();
+            const minutos = dataExemplo.getMinutes();
+            const point = {};
+            
+            console.log('marcação realizada com suceso!');
+            
+            return true;
+
+        } catch (error) {
+            throw new Error('Não foi possíve realizar marcação!');
+        };
+    }
 }
 
 const annualLeaveClass = new AnnualLeave();
+
+document.getElementById('marker-button')?.addEventListener('click', () => {
+    annualLeaveClass.setMaker();
+});
