10b2e67455b088a4b8438c73fe457c1d4a897872
Daniel de Souza Cordeiro <Daniel.Cordeiro@ext.rumolog.com>
2025-06-25 11:50:14 -0300

    <feat>[Register]: component base register

--- login/js/login.js
+++ login/js/login.js
@@ -5,11 +5,10 @@ class Login {
 
     verifyIsLogin() {
         try {
-            console.log("buscar token");
+            console.log('buscar token');
             return true;
         } catch (error) {
-            console.log("não está logado", error);
-            return false;
+            throw new Error('Não está logado!', error);
         }
     }
 
@@ -18,25 +17,24 @@ class Login {
             // Aqui eu salvo o objeto que criei salvando email e senha do usuário.
             const credentialsUser = this.getCredentials();
 
-            console.log("login is on", credentialsUser);
+            console.log('login is on', credentialsUser);
 
-            navigator.
+            
 
             return true;
         } catch (error) {
-            console.log("Falha ao efetuar Login", error);
-            return false;
+            throw new Error('Falha ao efetuar login', error);
         }
     }
 
     getCredentials() {
         // aqui eu busco os valores digitados pelo usuáio e guardados no HTML
-        const emailInput = document.getElementById("email");
-        const passwordInput = document.getElementById("password");
+        const emailInput = document.getElementById('email');
+        const passwordInput = document.getElementById('password');
 
         // verifico se os valores estão sendo salvos na variável
         if (!emailInput || !passwordInput) {
-            throw new Error("Campos de e-mail ou senha não encontrados no DOM.");
+            throw new Error('Campos de e-mail ou senha não encontrados no DOM.');
         }
 
         // transformo o valor salvo em .value em padrão de texto
@@ -45,13 +43,11 @@ class Login {
 
         // verifico se e-mail ou senha não estão vazio ou indefinido
         if (email == '' || email === undefined) {
-            console.log("E-mail está vazio ou não definido corretamente.");
-            return;
+            throw new Error('E-mail está vazio ou não definido corretamente.');
         }
 
         if (password == '' || password === undefined) {
-            console.log("Senha está vazio ou não definido corretamente.");
-            return;
+            throw new Error('Senha está vazio ou não definido corretamente.');
         }
 
         // após todas essas validações, crio um objeto e salvo os valores em texto.
@@ -66,7 +62,7 @@ class Login {
 
 const AppLogin = new Login();
 
-document.getElementById('loginButton')?.addEventListener("click", (event) => {
+document.getElementById('login-button')?.addEventListener('click', (event) => {
     event.preventDefault(); // evita o envio do formulário
     AppLogin.login();
 });
---
+++ register/js/register.js
@@ -0,0 +1,60 @@
+class RegisterUser {
+    registering() {
+        try {
+            // Aqui eu salvo o objeto que criei salvando email e senha do usuário.
+            const credentialsUser = this.getCredentials();
+
+            console.log("register is on", credentialsUser);
+            return true;
+        } catch (error) {
+            console.log("Falha ao efetuar Login", error);
+            return false;
+        }
+    }
+
+    getCredentials() {
+        // aqui eu busco os valores digitados pelo usuáio e guardados no HTML
+        const nameInput = document.getElementById('name');
+        const emailInput = document.getElementById('email');
+        const passwordInput = document.getElementById('password');
+
+        // verifico se os valores estão sendo salvos na variável
+        if (!nameInput || !emailInput || !passwordInput) {
+            throw new Error('Campos de e-mail ou senha não encontrados no DOM.');
+        }
+
+        // transformo o valor salvo em .value em padrão de texto
+        const name = String(nameInput.value);
+        const email = String(emailInput.value);
+        const password = String(passwordInput.value);
+
+        // verifico se e-mail ou senha não estão vazio ou indefinido
+        if (name == '' || name === undefined) {
+            throw new Error('Nome está vazio ou não definido corretamente.');
+        }
+
+        if (email == '' || email === undefined) {
+            throw new Error('E-mail está vazio ou não definido corretamente.');
+        }
+
+        if (password == '' || password === undefined) {
+            throw new Error('Senha está vazio ou não definido corretamente.');
+        }
+
+        // após todas essas validações, crio um objeto e salvo os valores em texto.
+        const credentialsUser = {
+            name: name,
+            email: email,
+            password: password,
+        };        
+
+        return credentialsUser;
+    }
+}
+
+const AppLogin = new RegisterUser();
+
+document.getElementById('login-button')?.addEventListener('click', (event) => {
+    event.preventDefault(); // evita o envio do formulário
+    AppLogin.registering();
+});
