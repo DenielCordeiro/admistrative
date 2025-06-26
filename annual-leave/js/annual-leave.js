aea73061c5845afb73c6324bd6bb4194dc835e06
Daniel de Souza Cordeiro <Daniel.Cordeiro@ext.rumolog.com>
2025-06-26 18:03:23 -0300

    <feat>[AnnualLeave]: function marker hours late and overtime

--- annual-leave/js/annual-leave.js
+++ annual-leave/js/annual-leave.js
@@ -1,18 +1,32 @@
-class AnnualLeave {
-    constructor() {
-        console.log('Classe banco de horas iniciada!');
-        
+class AnnualLeave {    
+    constructor(totalHoursLate, totalOvertime, hours) {        
+        hours = {
+            totalHoursLate: this.hoursLate(),       
+            totalOvertime: this.overtime(),
+            totalHours: totalOvertime - totalHoursLate,
+        };
+
+        console.log("Horas: ", hours);        
+    }
+
+    hoursLate() {
+        return 5;
+    }
+
+    overtime() {
+        return 3;
     }
 
     setMaker() {
         try {
-            const dataExemplo = new Date.now();
-            const hourMarked = dataExemplo.getHours();
-            const minutos = dataExemplo.getMinutes();
-            const point = {};
+            const dataExemplo = new Date();
             
-            console.log('marcação realizada com suceso!');
-            
+            const point = {
+                hourMarked: dataExemplo.getHours(),
+                minutesMarked: dataExemplo.getMinutes(),
+            };
+
+            console.log("hora e minuto marcado: ", point);            
             return true;
 
         } catch (error) {
